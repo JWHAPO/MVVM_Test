a90c63cf0e8fd8c26282b0014eea3d0675ec6aad
JWHAPO <kim2101714@gmail.com>
2019-02-11 14:28:23 +0900

    init mvvm test git

---
+++ app/src/main/java/test/jw/mvvm/view/MainActivity.kt
@@ -0,0 +1,17 @@
+package test.jw.mvvm.view
+
+import android.app.Activity
+import android.os.Bundle
+
+/**
+ * MVVM_Test
+ * Class: MainActivity
+ * Created by JEONGWOOKIM on 2019-02-11.
+ * Description:
+ */
+
+class MainActivity:Activity(){
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
